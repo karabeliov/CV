321bed616d55cda929969c4bb727244b95178802
Kristian Karabeliov <kristian@karabeliov.com>
2016-04-18 20:41:58 +0300

    add collapse function v2.2

--- CV/scripts/custom.js
+++ CV/scripts/custom.js
@@ -2,10 +2,33 @@
   $('.hideItem').hide();
 }());
 function showDetails() {
-  //$(id).toggle();
-  //$(this).parent().siblings()[0].toggle();
   var that = this;
    $(function(){
        $( that ).parent().siblings(".hideItem").toggle();
    });
+}
+
+function showMore(id) {
+  $(id).toggle();
+}
+
+function collapse() {
+  var that = this;
+   $(function(){
+       $( that ).siblings(".hideItem").toggle();
+   });
+}
+
+function showAll() {
+  $('.panel-primary').children('.hideItem').show();
+
+  $('#hideAll').show();
+  $('#showAll').hide();
+}
+
+function hideAll() {
+  $('.hideItem').hide();
+
+  $('#hideAll').hide();
+  $('#showAll').show();
 }
